d4aa78ccb819bb0693724c8760e38758ad53f418
sakshamorsmthidk <jindalsaksham79@gmail.com>
2025-04-01 14:46:14 +0530

    Added oauth configuration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,96 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-
+	"github.com/joho/godotenv"
+	"os"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
+
+func init() {
+    godotenv.Load() // Automatically loads variables from .env into os.Getenv()
+}
+
+var oauthConfig = &oauth2.Config{
+    ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
+    ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+    RedirectURL:  "http://localhost:8080/auth/google/callback",
+    Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
+    Endpoint:     google.Endpoint,
+}
+
+var oauthState = "randomstate"
 
 func main() {
 	r := gin.Default()
 
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Welcome to the Recruitment Portal",
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Sign in with google",
 		})
+	})
+
+	r.GET("/auth/google", func(c *gin.Context) {
+		url := oauthConfig.AuthCodeURL(oauthState)
+		c.Redirect(http.StatusTemporaryRedirect, url)
+	})
+
+	r.GET("/auth/google/callback", func(c *gin.Context) {
+		handleGoogleCallback(c)
 	})
 
 	fmt.Println("Server is running on http://localhost:8080")
 	r.Run(":8080")
+}
+
+func handleGoogleCallback(c *gin.Context) {
+	state := c.Query("state")
+	if state != oauthState {
+		log.Println("Invalid Oauth state")
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	code := c.Query("code")
+	if code == "" {
+		log.Println("Authorization code not found")
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	token, err := oauthConfig.Exchange(context.Background(), code)
+	if err != nil {
+		log.Println("Failed to exchange authorization code with token: ", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	client := oauthConfig.Client(context.Background(), token)
+	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	if err != nil {
+		log.Println("Failed to fetch user info: ", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	var userInfo map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+		log.Println("Failed to decode user info: ", err)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"user": userInfo,
+	})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/sakshamorsmthidk/recruitment-portal
 go 1.24.0
 
 require (
+	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	github.com/bytedance/sonic v1.13.2 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
@@ -14,6 +15,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.25.0 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -26,6 +28,7 @@ require (
 	golang.org/x/arch v0.15.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/oauth2 v0.28.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
